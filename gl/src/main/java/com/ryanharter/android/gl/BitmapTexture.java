91dfaeee9378b2e14d90e09e2735c13a171c1e94
Ryan Harter <ryanjharter@gmail.com>
2016-08-04 11:52:26 -0500

    Adds more control to BitmapTexture construction.

--- gl/src/main/java/com/ryanharter/android/gl/BitmapTexture.java
+++ gl/src/main/java/com/ryanharter/android/gl/BitmapTexture.java
@@ -5,6 +5,7 @@ import android.graphics.Bitmap;
 import static android.opengl.GLES20.GL_CLAMP_TO_EDGE;
 import static android.opengl.GLES20.GL_LINEAR;
 import static android.opengl.GLES20.GL_OUT_OF_MEMORY;
+import static android.opengl.GLES20.GL_RGBA;
 import static android.opengl.GLES20.GL_TEXTURE_2D;
 import static android.opengl.GLES20.GL_TEXTURE_MAG_FILTER;
 import static android.opengl.GLES20.GL_TEXTURE_MIN_FILTER;
@@ -27,19 +28,23 @@ public class BitmapTexture extends Texture {
   }
 
   public BitmapTexture(Bitmap bitmap, boolean mipmap) {
+    this(bitmap, GL_LINEAR, GL_CLAMP_TO_EDGE, GL_RGBA, mipmap);
+  }
+
+  public BitmapTexture(Bitmap bitmap, int filter, int wrap, int format, boolean mipmap) {
     super();
     bind(0);
 
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, filter);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, filter);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, wrap);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, wrap);
 
     // attempt to load the bitmap with backouts
     int attempts = 0;
     boolean loaded = false;
     while (!loaded && attempts < 3) {
-      texImage2D(GL_TEXTURE_2D, 0, bitmap, 0);
+      texImage2D(GL_TEXTURE_2D, 0, format, bitmap, 0);
 
       if (mipmap) {
         glGenerateMipmap(GL_TEXTURE_2D);
